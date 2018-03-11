e77d3863a13e08ddeffaedd84f8adffe5526a275
lunargorge <lunargorge@gmail.com>
2018-03-11 12:22:12 +0100

    remove files

--- src/app/examples/http/shared/album-api/service/post.service.spec.ts
+++ src/app/examples/http/shared/album-api/service/post.service.spec.ts
@@ -1,15 +1,19 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { PostService } from './post.service';
+import { PostRepository } from '../repository/post.repository';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PostEntity } from '../entity/post.entity';
 
 describe('PostService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [PostService]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [PostService, PostRepository, PostRepository, PostEntity]
+        });
     });
-  });
 
-  it('should be created', inject([PostService], (service: PostService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', inject([PostService], (service: PostService) => {
+        expect(service).toBeTruthy();
+    }));
 });
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,6 +1,16 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
+declare var require: NodeRequire;
 interface NodeModule {
   id: string;
 }
-declare var require: any;
+interface NodeRequireFunction {
+    (id: string): any;
+}
+
+interface NodeRequire extends NodeRequireFunction {
+    resolve(id: string): string;
+    cache: any;
+    extensions: any;
+    main: NodeModule | undefined;
+}
